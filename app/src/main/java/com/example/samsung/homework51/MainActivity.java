736615b323b544b148f5602bd8c40287eeab7640
BoberPro <34961963+BoberPro@users.noreply.github.com>
2018-02-24 20:58:55 +0500

    qwerty
    qwerty

---
+++ app/src/main/java/com/example/samsung/homework51/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.samsung.homework51;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyMonthAdapter adapter = new MyMonthAdapter(this, makeMonth());
+        ListView lv = (ListView) findViewById(R.id.ListView);
+        lv.setAdapter(adapter);
+
+    }
+
+    MyMonth[] makeMonth() {
+        MyMonth[] arr = new MyMonth[12];
+
+        String[] monthArr = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+
+        double[] tempArr = {-12.7, -11.3, -4.5, 7.7, 19.3, 23.9, 23.5, 22.8, 16.0, 5.2, -0.3, -9.3};
+
+        int[] dayArr = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+
+        for (int i = 0; i < arr.length; i++) {
+            MyMonth month = new MyMonth();
+            month.month = monthArr[i];
+            month.temp = tempArr[i];
+            month.days = dayArr[i];
+            arr[i] = month;
+        }
+        return arr;
+    }
+}
---
+++ app/src/main/java/com/example/samsung/homework51/MyMonth.java
@@ -0,0 +1,8 @@
+package com.example.samsung.homework51;
+
+public class MyMonth {
+    String month = ""; // Название месяца
+    double temp = 0.; // Средняя температура
+    int days = 0; // Количество дней
+    boolean like = true; // Нравится месяц
+}
